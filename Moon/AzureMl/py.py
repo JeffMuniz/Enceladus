74dfdece29cfa4c425c735f786739c0961925e42
JeffMuniz <jmuniz1985@gmail.com>
2021-02-07 16:12:30 -0300

    ffffsaf

---
+++ Moon/AzureMl/py.py
@@ -0,0 +1,28 @@
+
+pip install --upgrade pip
+
+
+python -m venv c:\moon\MachPy
+C:\moon\MachPy\Scripts
+.\activate
+pip install --upgrade pip
+pip install pandas pandas-datareader  plotly
+
+pip install pandas-datareader
+pip install plotly
+
+
+
+
+servidor cotaçoes com panda
+
+
+ce.syscbroker.com.br    10003
+he.syscbroker.com.br    10005
+ee.syscbroker.com.br    10007
+
+
+
+c.syscbroker.com.br/    443
+h.syscbroker.com.br    443
+e.syscbroker.com.br    443
